b50fe96e8579765b56943781a90b54858c4a860c
meowdymomo <meowdymomo@gmail.com>
2024-11-25 21:34:37 +0100

    the x situation

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -1,76 +1,84 @@
-import { BaseScene } from "@/scenes/BaseScene";
-import { Player } from "@/components/Player";
-import { UI } from "@/components/UI";
-import { Button } from "@/components/elements/Button";
+import {BaseScene} from "@/scenes/BaseScene";
+import {Player} from "@/components/Player";
+import {UI} from "@/components/UI";
+import {Button} from "@/components/elements/Button";
 import GameObject = Phaser.GameObjects.GameObject;
 
 export class GameScene extends BaseScene {
 	private background: Phaser.GameObjects.Image;
-  private dragon: Phaser.GameObjects.Image;
-  private frontKobolds: Phaser.GameObjects.Image;
-  private backKobolds: Phaser.GameObjects.Image;
+	private dragon: Phaser.GameObjects.Image;
+	private frontKobolds: Phaser.GameObjects.Image;
+	private backKobolds: Phaser.GameObjects.Image;
 	private player: Player;
 	private ui: UI;
-
+	private line: any;
+	private pathGraphics: any;
+	private currentPath: Array<Phaser.GameObjects.GameObject>;
+	private lineStartPosition = {x: 0, y: 0};
 	private nbPoints = 6;
 	private pointImage: Phaser.GameObjects.Sprite;
+	private isDragging = false;
 
 	constructor() {
-		super({ key: "GameScene" });
+		super({key: "GameScene"});
 	}
 
 	create(): void {
-    const dragonX: number = 0;
-    const dragonY: number = 100;
-    const numberOfKobolds: number = 5;
+		const dragonX: number = 0;
+		const dragonY: number = 100;
+		const numberOfKobolds: number = 5;
+
+		this.line = this.add.line(0, 0, 0, 0, 100, 100, 0xffffff).setOrigin(0);
+		this.line.setLineWidth(5);
+		this.line.visible = false;
+
+		this.pathGraphics = this.add.graphics();
 
 		this.fade(false, 200, 0x000000);
 
 		this.background = this.add.image(0, 0, "background")
-    const frontRow = Math.ceil(numberOfKobolds/2)
-    const backRow = Math.floor(numberOfKobolds/2)
+		const frontRow = Math.ceil(numberOfKobolds / 2)
+		const backRow = Math.floor(numberOfKobolds / 2)
 
-    for ( let i=0; i < frontRow; i++) {
-      let offset = (this.W/frontRow)/2
-      this.frontKobolds = this.add.image(i*this.W/frontRow + offset,500,"kobold")
-      this.frontKobolds.setOrigin(0.5,0)
-      this.frontKobolds.scale = 0.3
-    }
-    for (let i=0; i < backRow; i++) {
-      this.backKobolds = this.add.image((i+1)*this.W/frontRow,450,"kobold")
-      this.backKobolds.setOrigin(0.5,0)
-      this.backKobolds.scale = 0.25
-    }
-    
+		for (let i = 0; i < frontRow; i++) {
+			let offset = (this.W / frontRow) / 2
+			this.frontKobolds = this.add.image(i * this.W / frontRow + offset, 500, "kobold")
+			this.frontKobolds.setOrigin(0.5, 0)
+			this.frontKobolds.scale = 0.3
+		}
+		for (let i = 0; i < backRow; i++) {
+			this.backKobolds = this.add.image((i + 1) * this.W / frontRow, 450, "kobold")
+			this.backKobolds.setOrigin(0.5, 0)
+			this.backKobolds.scale = 0.25
+		}
 		this.dragon = this.add.image(this.CX, 300, "dragon");
 		this.dragon.setScale(0.8);
-		this.background.setOrigin(0,0);
-    
+		this.background.setOrigin(0, 0);
+
 		const startX: number = this.CX;
 		const startY: number = 900;
 		const distance: number = 200;
 
 		this.fitToScreen(this.background);
-		/*for (let i = 0; i < this.nbPoints; i++) {
+		for (let i = 0; i < this.nbPoints; i++) {
 			const p = (Math.PI * 2) * (i / this.nbPoints);
 			const x = startX + Math.cos(p) * distance;
 			const y = startY + Math.sin(p) * distance;
 
 
 			console.log();
-			/*const btn = new Button(this, x, y);
+			const btn = new Button(this, x, y);
 			this.pointImage = new Phaser.GameObjects.Sprite(this, 0, 0, "circle");
 			btn.setSize(100, 100);
 			btn.setInteractive();
 			btn.add(this.pointImage);
-			btn.on("pointerdown", () => console.log("test"));
-		}*/
+		}
 
-		for (let i = 0; i < 6; i++) {
+		/*for (let i = 0; i < 6; i++) {
 			const p = (Math.PI * 2) * (i / 6);
 			const circle = this.add.circle(startX + Math.cos(p) * distance, startY + Math.sin(p) * distance, 50, 0xFF0000);
 			circle.setInteractive();
-		}
+		}*/
 
 		this.input.on("pointerdown", () => {
 			console.log("pointer");
@@ -97,11 +105,69 @@ export class GameScene extends BaseScene {
 
 		this.input.on('pointermove', (pointer: Phaser.Input.Pointer) => {
 		});
-		this.input.on("pointerdown", this.onPointerDown, this);
+		this.input.on("pointerdown", this.startDrag, this);
+		this.input.on('pointermove', this.moveDrag, this);
 	}
 
-	onPointerDown(pointer: Phaser.Input.Pointer, currentlyOver: Phaser.GameObjects.GameObject[]): void {
-		console.log(pointer, currentlyOver);
+	startDrag(pointer: Phaser.Input.Pointer, gameObjects: GameObject[]): void {
+		this.isDragging = true;
+
+		//console.log(gameObjects);
+
+		// initialize Path
+		this.currentPath = [gameObjects[0]];
+		console.log(this.currentPath[0].x);
+		console.log(this.currentPath);
+
+		/*console.log(this.currentPath);
+		// draw/save last segment of the path
+
+
+		this.lineStartPosition.x = gameObjects[0].x;
+		this.lineStartPosition.y = gameObjects[0].y;
+*/
+
+		//console.log(this.lineStartPosition);
+		// @ts-ignore
+		this.line.x = gameObjects[0].x;
+		// @ts-ignore
+		this.line.y = gameObjects[0].y;
+		this.line.setTo(0, 0, 0, 0);
+		this.line.visible = true;
+	}
+
+	moveDrag(pointer: Phaser.Input.Pointer, gameObjects: Phaser.GameObjects.GameObject[]): void {
+		if (this.isDragging) {
+			// Check If Circle is allowed to be added, to path
+			// Here you would also check if the line is horizontal or vertical  (this part is currently not implemented)
+			if (gameObjects[0] && this.currentPath.indexOf(gameObjects[0]) === -1) {
+				this.currentPath.push(gameObjects[0]);
+
+				/*this.line.x = gameObjects[0].x;
+				this.line.y = gameObjects[0].y;
+
+				this.lineStartPosition.x = gameObjects[0].x;
+				this.lineStartPosition.y = gameObjects[0].y;*/
+
+			}
+			this.line.setTo(0, 0, pointer.x - this.lineStartPosition.x, pointer.y - this.lineStartPosition.y);
+			this.drawPath();
+		}
+	}
+
+	drawPath() {
+		this.pathGraphics.clear();
+		if (this.currentPath.length > 0) {
+			this.pathGraphics.lineStyle(10, 0xffffff);
+			// @ts-ignore
+			let path = new Phaser.Curves.Path(this.currentPath[0].x, this.currentPath[0].y);
+			for (let idx = 1; idx < this.currentPath.length; idx++) {
+				let point = this.currentPath[idx];
+				// @ts-ignore
+				path.lineTo(point.x, point.y);
+			}
+			path.draw(this.pathGraphics);
+		}
 	}
 
 }
