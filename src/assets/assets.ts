06c4ea040588668ee75da9b1242358f99c5a24e9
Dan-Eric Næss <dan-eric.naess@live.no>
2024-11-27 00:12:30 +0100

    Remove player.png from assets

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -1,49 +1,40 @@
-import { Image, SpriteSheet, Audio } from './util';
-import { image, sound, music, loadFont, spritesheet } from './util';
-
+import { Image, SpriteSheet, Audio } from "./util";
+import { image, sound, music, loadFont, spritesheet } from "./util";
 
 /* Images */
 const images: Image[] = [
-	// Backgrounds
-	image('backgrounds/background', 'background'),
-	image('circle', 'circle'),
+  // Backgrounds
+  image("backgrounds/background", "background"),
+  image("circle", "circle"),
 
-	// Characters
-	image('characters/player', 'player'),
-	image('characters/dragon_walk_01', 'dragon'),
-  image('characters/kobold', 'kobold'),
+  // Characters
+  image("characters/dragon_walk_01", "dragon"),
+  image("characters/kobold", "kobold"),
 
+  // Items
+  image("items/coin", "coin"),
 
-	// Items
-	image('items/coin', 'coin'),
+  // UI
+  image("ui/hud", "hud"),
 
-	// UI
-	image('ui/hud', 'hud'),
-
-	// Titlescreen
-	image('titlescreen/sky', 'title_sky'),
-	image('titlescreen/background', 'title_background'),
-	image('titlescreen/foreground', 'title_foreground'),
-	image('titlescreen/character', 'title_character'),
+  // Titlescreen
+  image("titlescreen/sky", "title_sky"),
+  image("titlescreen/background", "title_background"),
+  image("titlescreen/foreground", "title_foreground"),
+  image("titlescreen/character", "title_character"),
 ];
 
 /* Spritesheets */
-const spritesheets: SpriteSheet[] = [
-
-];
+const spritesheets: SpriteSheet[] = [];
 
 /* Audios */
 const audios: Audio[] = [
-	music('title', 'm_main_menu'),
-	music('first', 'm_first'),
-	sound('tree/rustle', 't_rustle', 0.5),
+  music("title", "m_main_menu"),
+  music("first", "m_first"),
+  sound("tree/rustle", "t_rustle", 0.5),
 ];
 
 /* Fonts */
-await loadFont('Sketch', 'Game Font');
+await loadFont("Sketch", "Game Font");
 
-export {
-	images,
-	spritesheets,
-	audios
-};
+export { images, spritesheets, audios };
