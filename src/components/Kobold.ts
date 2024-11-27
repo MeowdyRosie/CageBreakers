63db0cd8fa7560564eb8281951233d2d622b4073
Dan-Eric Næss <dan-eric.naess@live.no>
2024-11-27 13:22:18 +0100

    Animate kobold

--- src/components/Kobold.ts
+++ src/components/Kobold.ts
@@ -27,7 +27,7 @@ const patterns = [
 
 export class Kobold extends Phaser.GameObjects.Container {
   public declare scene: BaseScene;
-  private kobold: Phaser.GameObjects.Image;
+  private kobold: Phaser.GameObjects.Sprite;
   private circle: MagicCircle;
   private spellEdges: string[];
 
@@ -36,9 +36,20 @@ export class Kobold extends Phaser.GameObjects.Container {
     scene.add.existing(this);
 
     this.kobold = scene.add
-      .image(x, y, "kobold")
+      .sprite(x, y, "kobold")
       .setScale(scale)
       .setOrigin(0.5, 0);
+
+    scene.anims.create({
+      key: "idle",
+      frames: scene.anims.generateFrameNames("kobold", {
+        frames: [0, 1],
+      }),
+      frameRate: 2,
+      repeat: -1,
+    });
+
+    this.kobold.playAfterDelay("idle", Math.random() * 1000);
 
     this.circle = new MagicCircle(scene, x, y, 50, scale, false);
     const lockPattern = patterns[Math.floor(Math.random() * patterns.length)];
