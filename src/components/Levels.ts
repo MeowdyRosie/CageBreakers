2fc7fa7326b20ba8e85045aa8387f0a96df1da18
Dan-Eric Næss <dan-eric.naess@live.no>
2024-11-28 22:59:35 +0100

    Integrate levels object

--- src/components/Levels.ts
+++ src/components/Levels.ts
@@ -1,63 +1,54 @@
-import { BaseScene } from "@/scenes/BaseScene";
-
-export class Levels extends Phaser.GameObjects.Container {
-  public declare scene: BaseScene;
+export class Level {
   public patterns: number;
   public cages: number;
   public time: number;
 
-  constructor(
-    scene: BaseScene,
-    difficulty: string = "easy",
-    level: number = 1
-  ) {
-    super(scene)
-    scene.add.existing(this);
-    this.patterns = this.getPatterns(level,difficulty)
-    this.time = this.getTime(level,difficulty)
-    this.cages = this.getCages(level,difficulty)
+  constructor(difficulty: string = "easy", level: number = 1) {
+    this.patterns = this.getPatterns(level, difficulty);
+    this.time = this.getTime(level, difficulty);
+    this.cages = this.getCages(level, difficulty);
   }
 
   getPatterns(level: number = 1, difficulty: string = "easy") {
     // every 5 levels we get one pattern more, capped at 3
-    let patternMod = 5
-    let cap = 3
+    let patternMod = 5;
+    let cap = 3;
     if (difficulty == "medium") {
-      cap = 4
+      cap = 4;
     }
     if (difficulty == "hard") {
-      cap = 5
-      patternMod = 4
+      cap = 5;
+      patternMod = 4;
     }
-    return Math.min(cap,Math.ceil(level/patternMod))
+    return Math.min(cap, Math.ceil(level / patternMod));
   }
 
   getTime(level: number = 1, difficulty: string = "easy") {
     // default time 2 mins, every level we cull 2s until 20s
-    let time = 120
-    let timeMod = 1
-    let timeStep = 2
-    let cap = 20
+    let time = 120;
+    let timeMod = 1;
+    let timeStep = 2;
+    let cap = 20;
     if (difficulty == "medium") {
-      timeMod = 2
+      timeMod = 2;
     }
     if (difficulty == "hard") {
-      timeMod = 3
-      timeStep = 1
-      cap = 10
+      timeMod = 3;
+      timeStep = 1;
+      cap = 10;
     }
-    return Math.max(cap,time/timeMod-level*timeStep)
+    return Math.max(cap, time / timeMod - level * timeStep) * 1000;
   }
 
   getCages(level: number = 1, difficulty: string = "easy") {
-    let cageMod = 7
-    let cap = 3
+    let cageMod = 7;
+    let cap = 3;
     if (difficulty == "medium") {
-      cap = 5
+      cap = 5;
     }
     if (difficulty == "hard") {
-      cap = 7
+      cap = 7;
     }
-    return Math.min(cap,level/cageMod)
+    return Math.min(cap, level / cageMod);
   }
 }
--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -1,124 +1,126 @@
-import {BaseScene} from "@/scenes/BaseScene";
+import { BaseScene } from "@/scenes/BaseScene";
 import MagicCircle from "@/components/MagicCircle";
-import {Kobold} from "@/components/Kobold";
+import { Kobold } from "@/components/Kobold";
 import TimerEvent = Phaser.Time.TimerEvent;
 import Timeline = Phaser.Time.Timeline;
-import {Dragon} from "@/components/Dragon";
-import {Levels} from "@/components/Levels"
+import { Dragon } from "@/components/Dragon";
+import { Level } from "@/components/Levels";
 
 export class GameScene extends BaseScene {
-	private background: Phaser.GameObjects.Image;
-	//private dragon: Phaser.GameObjects.Image;
-	private kobolds: Kobold[];
-	private dragon: Dragon;
-	private circle: MagicCircle;
-	private currentLevel: number = 0;
-	private timer: TimerEvent;
-	private levels: Levels[];
-  
+  private background: Phaser.GameObjects.Image;
+  //private dragon: Phaser.GameObjects.Image;
+  private kobolds: Kobold[];
+  private dragon: Dragon;
+  private circle: MagicCircle;
+  private currentLevel: number = 0;
+  private timer: TimerEvent;
+  private level: Level;
+
   // = [
-	// 	{
-	// 		cages: 3,
-	// 		time: 10,
-	// 		patterns: 2
-	// 	},
-	// 	{
-	// 		cages: 6,
-	// 		time: 60,
-	// 		patterns: 2
-	// 	},
-	// ]
+  // 	{
+  // 		cages: 3,
+  // 		time: 10,
+  // 		patterns: 2
+  // 	},
+  // 	{
+  // 		cages: 6,
+  // 		time: 60,
+  // 		patterns: 2
+  // 	},
+  // ]
 
-	constructor() {
-		super({key: "GameScene"});
-		this.kobolds = [];
-    this.levels = [];
-	}
+  constructor() {
+    super({ key: "GameScene" });
+    this.kobolds = [];
+    this.level = [];
+  }
 
-	create(): void {
+  create(): void {
+    this.fade(false, 200, 0x000000);
 
-		this.fade(false, 200, 0x000000);
+    this.background = this.add.image(this.CX, -300, "background");
 
-		this.background = this.add.image(this.CX, -300, "background");
+    this.dragon = new Dragon(this, this.CX, 300, 1);
+    this.level = new Level();
 
-		this.dragon = new Dragon(this, this.CX, 300, 1);
-    this.levels.push(new Levels(this))
-        // Dragon moving
-		this.background.setOrigin(0.5, 0);
-    this.background.setScale(0.5,0.5)
-		this.fitToScreen(this.background);
-		this.initTouchControls();
+    // Dragon moving
+    this.background.setOrigin(0.5, 0);
+    this.background.setScale(0.5, 0.5);
+    this.fitToScreen(this.background);
+    this.initTouchControls();
 
-		this.timer = this.time.addEvent({
-			delay: (1000 * this.levels[this.currentLevel].time) - 5, // ms
-			callback: () => {
-				this.dragon.stopIdle();
-				this.dragon.approaching();
-			},
-			//args: [],
-		});
-		this.setupGame();
-	}
+    this.timer = this.time.addEvent({
+      delay: this.level.getTime() - 5000, // ms
+      callback: () => {
+        this.dragon.stopIdle();
+        this.dragon.approaching();
+      },
+      //args: [],
+    });
+    this.setupGame();
+  }
 
-	setupGame() {
-		const frontRow = Math.ceil(this.levels[this.currentLevel].cages / 2);
-		const backRow = Math.floor(this.levels[this.currentLevel].cages / 2);
+  setupGame() {
+    const frontRow = Math.ceil(this.level.getCages() / 2);
+    const backRow = Math.floor(this.level.getCages() / 2);
 
-		for (let i = 0; i < backRow; i++) {
-			const x = ((1 + i) * this.W) / frontRow;
-			this.kobolds.push(new Kobold(this, x, 450, 0.2, this.levels[this.currentLevel].patterns));
-		}
+    for (let i = 0; i < backRow; i++) {
+      const x = ((1 + i) * this.W) / frontRow;
+      this.kobolds.push(
+        new Kobold(this, x, 450, 0.2, this.level.getPatterns())
+      );
+    }
 
-		for (let i = 0; i < frontRow; i++) {
-			const offset = this.W / frontRow / 2;
-			const x = (i * this.W) / frontRow + offset;
-			this.kobolds.push(new Kobold(this, x, 500, 0.3, this.levels[this.currentLevel].patterns));
-		}
+    for (let i = 0; i < frontRow; i++) {
+      const offset = this.W / frontRow / 2;
+      const x = (i * this.W) / frontRow + offset;
+      this.kobolds.push(
+        new Kobold(this, x, 500, 0.3, this.level.getPatterns())
+      );
+    }
 
-		this.timer = this.time.addEvent({
-			delay: this.levels[this.currentLevel].time * 1000, // ms
-			callback: this.gameOver,
-		});
+    this.timer = this.time.addEvent({
+      delay: this.level.getTime(), // ms
+      callback: this.gameOver,
+    });
 
-		this.circle = new MagicCircle(this, this.CX, 950, 200, 1, true);
-		this.circle.on("spell", (edges: string[]) => {
-			this.kobolds.forEach((kobold) => {
-				if (kobold.trySpell(edges)) {
-					kobold.patternsLeft--;
-					if (kobold.patternsLeft == 0) {
-						kobold.setFree();
-					} else {
-						kobold.setRandomSpellPattern();
-					}
-				}
-			});
-			if (this.kobolds.every(kobold => kobold.patternsLeft == 0)) {
-				this.currentLevel++;
-				this.kobolds.forEach(kobold => kobold.destroy());
-				this.setupGame();
-			}
-		});
-	}
+    this.circle = new MagicCircle(this, this.CX, 950, 200, 1, true);
+    this.circle.on("spell", (edges: string[]) => {
+      this.kobolds.forEach((kobold) => {
+        if (kobold.trySpell(edges)) {
+          kobold.patternsLeft--;
+          if (kobold.patternsLeft == 0) {
+            kobold.setFree();
+          } else {
+            kobold.setRandomSpellPattern();
+          }
+        }
+      });
+      if (this.kobolds.every((kobold) => kobold.patternsLeft == 0)) {
+        this.currentLevel++;
+        this.kobolds.forEach((kobold) => kobold.destroy());
+        this.setupGame();
+      }
+    });
+  }
 
-	finishGame(){
+  finishGame() {}
 
-	}
+  update(time: number, delta: number) {
+    //console.log(this.timer.getOverallRemainingSeconds());
+  }
 
-	update(time: number, delta: number) {
-		//console.log(this.timer.getOverallRemainingSeconds());
-	}
+  gameOver() {
+    console.log("Game Over");
+  }
 
-	gameOver() {
-		console.log("Game Over");
-	}
+  initTouchControls() {
+    this.input.addPointer(2);
 
-	initTouchControls() {
-		this.input.addPointer(2);
+    // let touchArea = this.add.rectangle(0, 0, this.W, this.H, 0xFFFFFF).setOrigin(0).setAlpha(0.001);
+    // touchArea.setInteractive({ useHandCursor: true, draggable: true });
 
-		// let touchArea = this.add.rectangle(0, 0, this.W, this.H, 0xFFFFFF).setOrigin(0).setAlpha(0.001);
-		// touchArea.setInteractive({ useHandCursor: true, draggable: true });
-
-		let touchId: number = -1;
-		let touchButton: number = -1;
-	}
+    let touchId: number = -1;
+    let touchButton: number = -1;
+  }
 }
