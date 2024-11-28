a988ebe0deb4b2d30694405f014aaa3909f173af
Dan-Eric Næss <dan-eric.naess@live.no>
2024-11-28 23:41:42 +0100

    Reload the scene on next level

--- src/components/Levels.ts
+++ src/components/Levels.ts
@@ -1,15 +1,21 @@
+export type Difficulty = "easy" | "medium" | "hard";
+
 export class Level {
   public patterns: number;
   public cages: number;
   public time: number;
+  public level: number;
+  public difficulty: Difficulty;
 
-  constructor(difficulty: string = "easy", level: number = 1) {
+  constructor(difficulty: Difficulty = "easy", level: number = 1) {
     this.patterns = this.getPatterns(level, difficulty);
     this.time = this.getTime(level, difficulty);
     this.cages = this.getCages(level, difficulty);
+    this.level = level;
+    this.difficulty = difficulty;
   }
 
-  getPatterns(level: number = 1, difficulty: string = "easy") {
+  getPatterns(level: number = 1, difficulty: Difficulty = "easy") {
     // every 5 levels we get one pattern more, capped at 3
     let patternMod = 5;
     let cap = 3;
@@ -23,7 +29,7 @@ export class Level {
     return Math.min(cap, Math.ceil(level / patternMod));
   }
 
-  getTime(level: number = 1, difficulty: string = "easy") {
+  getTime(level: number = 1, difficulty: Difficulty = "easy") {
     // default time 2 mins, every level we cull 2s until 20s
     let time = 120;
     let timeMod = 1;
@@ -40,7 +46,7 @@ export class Level {
     return Math.max(cap, time / timeMod - level * timeStep) * 1000;
   }
 
-  getCages(level: number = 1, difficulty: string = "easy") {
+  getCages(level: number = 1, difficulty: Difficulty = "easy") {
     let cageMod = 7;
     let cap = 3;
     if (difficulty == "medium") {
--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -1,12 +1,16 @@
-import {BaseScene} from "@/scenes/BaseScene";
+import { BaseScene } from "@/scenes/BaseScene";
 import MagicCircle from "@/components/MagicCircle";
-import {Kobold} from "@/components/Kobold";
+import { Kobold } from "@/components/Kobold";
 import TimerEvent = Phaser.Time.TimerEvent;
 import Timeline = Phaser.Time.Timeline;
-import {Dragon} from "@/components/Dragon";
-import {Level} from "@/components/Levels";
-import {UI} from "@/components/UI";
+import { Dragon } from "@/components/Dragon";
+import { Difficulty, Level } from "@/components/Levels";
+import { UI } from "@/components/UI";
 
+type GameSceneData = {
+  level: number;
+  difficulty: Difficulty;
+};
 export class GameScene extends BaseScene {
   private background: Phaser.GameObjects.Image;
   private kobolds: Kobold[];
@@ -18,19 +22,18 @@ export class GameScene extends BaseScene {
   private ui: UI;
   private level: Level;
 
-
   constructor() {
-    super({key: "GameScene"});
-    this.kobolds = [];
+    super({ key: "GameScene" });
   }
 
-  create(): void {
+  create({ level, difficulty }: GameSceneData): void {
+    this.kobolds = [];
     this.fade(false, 200, 0x000000);
 
     this.background = this.add.image(this.CX, -300, "background");
 
     this.dragon = new Dragon(this, this.CX, 300, 1);
-    this.level = new Level();
+    this.level = new Level(difficulty, level);
 
     // Dragon moving
     this.background.setOrigin(0.5, 0);
@@ -104,16 +107,13 @@ export class GameScene extends BaseScene {
     this.time.addEvent({
       delay: timeToFlee,
       callback: () => {
-        this.kobolds.forEach((kobold) => {
-          kobold.destroy();
-        });
-        this.setupGame();
+        const { difficulty, level } = this.level;
+        this.scene.restart({ difficulty, level: level + 1 } as GameSceneData);
       },
     });
   }
 
-  finishGame() {
-  }
+  finishGame() {}
 
   update(time: number, delta: number) {
     //console.log(this.timer.getOverallRemainingSeconds());
