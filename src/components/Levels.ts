45e861f0bc6021f926e688796a5f684676a50735
meowdymomo <meowdymomo@gmail.com>
2024-11-29 11:59:26 +0100

    Fixed timer

--- src/components/Levels.ts
+++ src/components/Levels.ts
@@ -31,7 +31,7 @@ export class Level {
 
   getTime() {
     // default time 30s(?), every level we cull 2s until 20s
-    let time = 30;
+    /*let time = 30;
     let timeMod = 1;
     let timeStep = 2;
     let cap = 20;
@@ -43,7 +43,8 @@ export class Level {
       timeStep = 1;
       cap = 10;
     }
-    return Math.max(cap, Math.floor(time / timeMod) - this.level * timeStep) * 1000;
+    return Math.max(cap, Math.floor(time / timeMod) - this.level * timeStep) * 1000;*/
+    return 15 * 1000;
   }
 
   getCages() {
